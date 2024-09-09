18ab8f7d40b27e7abf1395fe09ee3f6a9a0c088e
barami62 <wodbs7758@naver.com>
2024-09-09 11:40:17 +0900

    feat: JPA 설정 추가

---
+++ component/component-jpa/src/main/java/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+
+}
