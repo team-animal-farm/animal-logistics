7ed724fb6bbd41da5fa2eb367e3ffc5576b0ecbc
SunMi <wywudi@naver.com>
2024-09-11 02:29:04 +0900

    feat : type enum 생성

---
+++ application/auth/src/main/java/animal/domain/CompanyType.java
@@ -0,0 +1,21 @@
+package animal.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CompanyType {
+
+  PROVIDER(Type.PROVIDER),
+  RECEIVE(Type.RECEIVE);
+
+  private final String type;
+
+  public static class Type {
+
+    public static final String PROVIDER = "PROVIDER_COMPANY";
+    public static final String RECEIVE = "RECEIVE_COMPANY";
+
+  }
+}
