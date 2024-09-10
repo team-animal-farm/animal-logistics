3540e59ca229699a0471aa72d423b481d80bb010
barami62 <wodbs7758@naver.com>
2024-09-10 17:42:24 +0900

    enhance(hub): 허브 수정, 삭제 매퍼 추가

--- application/hub/src/main/java/animal/mapper/HubMapper.java
+++ application/hub/src/main/java/animal/mapper/HubMapper.java
@@ -3,8 +3,11 @@ package animal.mapper;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import animal.domain.Hub;
+import animal.dto.HubRequest.CreateHubReq;
 import animal.dto.HubResponse.CreateHubRes;
 import animal.dto.HubResponse.GetHubRes;
+import animal.dto.HubResponse.UpdateHubRes;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -20,4 +23,7 @@ public interface HubMapper {
 
     @Mapping(target = "id", source = "id.id")
     CreateHubRes toCreateHubRes(Hub hub);
+
+    @Mapping(target = "id", source = "id.id")
+    UpdateHubRes toUpdateHubRes(Hub hub);
 }
