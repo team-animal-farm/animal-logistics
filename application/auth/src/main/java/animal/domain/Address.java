45fc1890712560354f1cc0fe50c364ede9d2c076
SunMi <wywudi@naver.com>
2024-09-11 02:31:06 +0900

    refactor : column 속성 추가

--- application/auth/src/main/java/animal/domain/Address.java
+++ application/auth/src/main/java/animal/domain/Address.java
@@ -1,5 +1,6 @@
 package animal.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -13,15 +14,20 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class Address {
 
-    private String roadAddress;
-    private String detailAddress;
-    private String zipcode;
+  @Column(nullable = false)
+  private String roadAddress;
 
-    @Builder
-    private Address(String roadAddress, String detailAddress, String zipcode) {
-        this.roadAddress = roadAddress;
-        this.detailAddress = detailAddress;
-        this.zipcode = zipcode;
-    }
+  @Column(nullable = false)
+  private String detailAddress;
+
+  @Column(nullable = false)
+  private String zipcode;
+
+  @Builder
+  private Address(String roadAddress, String detailAddress, String zipcode) {
+    this.roadAddress = roadAddress;
+    this.detailAddress = detailAddress;
+    this.zipcode = zipcode;
+  }
 
 }
--- application/auth/src/main/java/animal/domain/User.java
+++ application/auth/src/main/java/animal/domain/User.java
@@ -1,34 +1,56 @@
 package animal.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
+import java.util.UUID;
 import jpa.BaseEntity;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Table(name="p_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "p_user")
+@SQLRestriction("deleted_at IS NULL")
 public class User extends BaseEntity {
 
-    @Id
-    private String username;
+  @Id
+  private String userName;
 
-    private String nickname;
+  @Column(nullable = false)
+  private UUID hubId;
 
-    private String email;
+  private String nickname;
 
-    private String phone;
+  @Column(unique = true)
+  private String email;
 
-    private String password;
+  private String phone;
 
-    private Address address;
+  private String password;
 
-    private UserRole role;
+  private Address address;
+
+  @Column(nullable = false)
+  private UserRole role;
+
+  @Builder
+  private User(UUID hubId, String nickname, String email, String phone, String password, Address address, UserRole role) {
+    this.hubId = hubId;
+    this.nickname = nickname;
+    this.email = email;
+    this.phone = phone;
+    this.password = password;
+    this.address = address;
+    this.role = role;
+  }
+
 }
