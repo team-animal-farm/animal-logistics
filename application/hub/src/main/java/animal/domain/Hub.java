46c5d8e7d694513d89d791e5a7337dd8a3775654
barami62 <wodbs7758@naver.com>
2024-09-11 16:18:19 +0900

    enhance(hub): Hub 도메인 비즈니스 로직 추가
    - 정보 수정
    - 재고 추가
    - 재고 단건 조회
    - 재고 삭제

--- application/hub/src/main/java/animal/domain/Hub.java
+++ application/hub/src/main/java/animal/domain/Hub.java
@@ -1,9 +1,14 @@
 package animal.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import jpa.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -34,8 +39,35 @@ public class Hub extends BaseEntity {
         this.coordinate = coordinate;
     }
 
+    /**
+     * 허브 정보 수정
+     */
     public void updateHubInfo(Address address, Coordinate coordinate) {
         this.address = address;
         this.coordinate = coordinate;
+    }
+
+    /**
+     * 재고 추가
+     */
+    public void addInventory(Inventory inventory) {
+        inventoryList.add(inventory);
+    }
+
+    /**
+     * 재고 단건 조회
+     */
+    public Optional<Inventory> getInventory(InventoryId inventoryId) {
+
+        return inventoryList.stream()
+            .filter(inventory -> inventory.getId().equals(inventoryId))
+            .findAny();
+    }
+
+    /**
+     * 재고 삭제
+     */
+    public void removeInventory(InventoryId inventoryId) {
+        inventoryList.removeIf(inventory -> inventory.getId().equals(inventoryId));
     }
 }
