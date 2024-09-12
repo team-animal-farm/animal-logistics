371ef9b1a733944c7a8e86779d6d77db6c9c13a0
SunMi <wywudi@naver.com>
2024-09-12 16:55:02 +0900

    config : 상위 모듈 컴포넌트 스캔범위 추가

--- application/auth/src/main/java/animal/auth/AuthApplication.java
+++ application/auth/src/main/java/animal/auth/AuthApplication.java
@@ -1,10 +1,13 @@
-package animal;
+package animal.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import security.JwtUtil;
 
 @SpringBootApplication
+@Import(JwtUtil.class)
 @EnableFeignClients
 public class AuthApplication {
 
