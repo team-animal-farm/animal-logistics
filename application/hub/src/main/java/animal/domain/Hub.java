09e0a40976cecbe40b501c720eff8a867e660d2f
barami62 <wodbs7758@naver.com>
2024-09-12 20:56:05 +0900

    feat(hub): 허브에 매니저 추가 로직 추가

--- application/hub/src/main/java/animal/domain/Hub.java
+++ application/hub/src/main/java/animal/domain/Hub.java
@@ -1,10 +1,19 @@
 package animal.domain;
 
+import animal.domain.manager.CompanyDeliveryManager;
+import animal.domain.manager.HubDeliveryManager;
+import animal.domain.manager.HubManager;
+import animal.domain.manager.ProviderCompanyManager;
 import animal.jpa.BaseEntity;
+import exception.GlobalException;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
@@ -14,12 +23,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import response.ErrorCase;
 
 @Getter
 @Entity
 @Table(name = "p_hub")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hub extends BaseEntity {
+
+    private static final int MAX_COMPANY_DELIVERY_MANAGER_COUNT = 10;
 
     @EmbeddedId
     private final HubId id = HubId.ofRandom();
@@ -32,6 +44,22 @@ public class Hub extends BaseEntity {
 
     @OneToMany(mappedBy = "hub", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
     private List<Inventory> inventoryList = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "p_hub_managaer", joinColumns = @JoinColumn(name = "hub_manager_id"))
+    private List<HubManager> hubManagerList = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "p_hub_delivery_managaer", joinColumns = @JoinColumn(name = "hub_delivery_manager_id"))
+    private List<HubDeliveryManager> hubDeliveryManagerList = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "p_company_delivery_managaer", joinColumns = @JoinColumn(name = "company_delivery_manager_id"))
+    private List<CompanyDeliveryManager> companyDeliveryManagerList = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "p_provider_company_manager", joinColumns = @JoinColumn(name = "provider_company_manager_id"))
+    private List<ProviderCompanyManager> providerCompanyManagerList = new ArrayList<>();
 
     @Builder
     private Hub(Address address, Coordinate coordinate) {
@@ -69,5 +97,41 @@ public class Hub extends BaseEntity {
      */
     public void removeInventory(InventoryId inventoryId) {
         inventoryList.removeIf(inventory -> inventory.getId().equals(inventoryId));
+    }
+
+    /**
+     * 허브 매니저 추가
+     */
+    public void addHubManager(HubManager hubManager) {
+        hubManagerList.add(hubManager);
+    }
+
+    /**
+     * 허브 배송 매니저 추가
+     */
+    public void addHubDeliveryManager(HubDeliveryManager hubDeliveryManager) {
+        hubDeliveryManagerList.add(hubDeliveryManager);
+    }
+
+    /**
+     * 업체 배송 매니저 추가
+     */
+    public void addCompanyDeliveryManager(CompanyDeliveryManager companyDeliveryManager) {
+        if (isCompanyDeliveryManagerOverLimit()) {
+            throw new GlobalException(ErrorCase.COMPANY_DELIVERY_MANAGER_OVER_LIMIT);
+        }
+
+        companyDeliveryManagerList.add(companyDeliveryManager);
+    }
+
+    private boolean isCompanyDeliveryManagerOverLimit() {
+        return companyDeliveryManagerList.size() >= MAX_COMPANY_DELIVERY_MANAGER_COUNT;
+    }
+
+    /**
+     * 생산 업체 매니저 추가
+     */
+    public void addProviderCompanyManager(ProviderCompanyManager providerCompanyManager) {
+        providerCompanyManagerList.add(providerCompanyManager);
     }
 }
