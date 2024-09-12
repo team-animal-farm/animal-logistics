bf465429bcee9daa74e03affecae77e10df2a072
barami62 <wodbs7758@naver.com>
2024-09-12 21:32:40 +0900

    refactor(hub): 슬랙 아이디의 유효성 검사 추가

--- application/hub/src/main/java/animal/domain/manager/SlackId.java
+++ application/hub/src/main/java/animal/domain/manager/SlackId.java
@@ -1,10 +1,13 @@
 package animal.domain.manager;
 
+import exception.GlobalException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.StringUtils;
+import response.ErrorCase;
 
 @Getter
 @Embeddable
@@ -15,6 +18,10 @@ public class SlackId {
     private String slackId;
 
     public static SlackId of(String slackId) {
+        if (!StringUtils.hasText(slackId)) {
+            throw new GlobalException(ErrorCase.INVALID_INPUT);
+        }
+
         SlackId id = new SlackId();
         id.slackId = slackId;
         return id;
