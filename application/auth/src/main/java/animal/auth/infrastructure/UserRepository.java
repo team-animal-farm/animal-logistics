9eb402be336b5dc87988ed18d2e50160ae489a57
SunMi <wywudi@naver.com>
2024-09-14 18:32:54 +0900

    refactor : @Repository 삭제

--- application/auth/src/main/java/animal/auth/infrastructure/UserRepository.java
+++ application/auth/src/main/java/animal/auth/infrastructure/UserRepository.java
@@ -2,9 +2,7 @@ package animal.auth.infrastructure;
 
 import animal.auth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
   boolean existsByEmail(String email);
