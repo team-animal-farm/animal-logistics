1a60d7d13d6df183a9a0f5bd7771ccee79268e3c
barami62 <wodbs7758@naver.com>
2024-09-15 15:43:28 +0900

    feat(hub): 스웨거 설정 추가

---
+++ application/hub/src/main/java/animal/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package animal.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        Components components = new Components();
+
+        return new OpenAPI()
+            .info(info())
+            .components(components);
+    }
+
+    private Info info() {
+        return new Info()
+            .title("Hub API")
+            .version("0.0.1");
+    }
+}
