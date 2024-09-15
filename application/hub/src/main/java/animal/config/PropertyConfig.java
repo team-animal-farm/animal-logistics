4ff5d2edc5fc63a249529f9b8417b289c2b84b7d
barami62 <wodbs7758@naver.com>
2024-09-15 17:03:35 +0900

    feat(hub): 프로퍼티 설정 객체 추가

---
+++ application/hub/src/main/java/animal/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package animal.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("animal")
+public class PropertyConfig {
+
+}
