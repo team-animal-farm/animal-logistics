b7fd13d5ce949f22896034dbb7e4ddc5512211de
barami62 <wodbs7758@naver.com>
2024-09-17 21:31:10 +0900

    feat(order): 배송 상태 ENUM 추가

---
+++ application/order/src/main/java/animal/application/order/domain/delivery/DeliveryStatus.java
@@ -0,0 +1,15 @@
+package animal.application.order.domain.delivery;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DeliveryStatus {
+    WAITING_AT_HUB("허브 대기중"),
+    MOVING_TO_HUB("허브 이동중"),
+    ARRIVED_AT_HUB("목적지 허브 도착"),
+    DELIVERING("배송중");
+
+    private final String status;
+}
