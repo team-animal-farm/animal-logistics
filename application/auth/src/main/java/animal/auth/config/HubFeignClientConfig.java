02cdb65f76111f20bd12f5497c1998bd466b5991
SunMi <wywudi@naver.com>
2024-09-17 13:53:32 +0900

    feat : feignClient 설정파일

---
+++ application/auth/src/main/java/animal/auth/config/HubFeignClientConfig.java
@@ -0,0 +1,12 @@
+package animal.auth.config;
+
+public class HubFeignClientConfig {
+/*  @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            requestTemplate.header("API-KEY", "123");
+            requestTemplate.header("USER-ROLE", "CUSTOMER");
+            requestTemplate.header("SERVER-PORT", serverPort);
+        };
+    }*/
+}
