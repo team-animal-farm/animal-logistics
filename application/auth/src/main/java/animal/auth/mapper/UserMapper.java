ef9434e31d7814eaedb3a049058987d2bfd042a2
SunMi <wywudi@naver.com>
2024-09-17 12:55:09 +0900

    feat : 배달담당자 변환 mapper추가

--- application/auth/src/main/java/animal/auth/mapper/UserMapper.java
+++ application/auth/src/main/java/animal/auth/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package animal.auth.mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 import animal.auth.domain.User;
+import animal.auth.dto.UserRequest.ModifyDeliveryUserReq;
 import animal.auth.dto.UserRequest.SignUpUserReq;
-import animal.auth.dto.UserResponse;
+import animal.auth.dto.UserRequest.UpdateDeliveryUserReq;
+import animal.auth.dto.UserResponse.UserRes;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
@@ -11,8 +13,10 @@ public interface UserMapper {
 
   User from(SignUpUserReq SignUpUserReq);
 
-  UserResponse.GetUserRes toGetUserResponse(User user, UserResponse.DeliveryUserRes deliveryUserRes);
+  default void updateUserFields(User user, UserRes userRes) {
+    userRes.updateUserRes(user);
+  }
 
-  UserResponse.GetUserRes toGetUserResponse(User user, UserResponse.CompanyUserRes companyUserRes);
+  UpdateDeliveryUserReq toUpdateDeliveryUserReq(ModifyDeliveryUserReq modifyDeliveryUserReq);
 
 }
