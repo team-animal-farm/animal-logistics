60298d5e541e40fd5f0ea0e18bc63be70ae2d98c
barami62 <wodbs7758@naver.com>
2024-09-17 23:50:34 +0900

    feat(notification): 프로퍼티 설정 객체 추가

---
+++ application/notification/src/main/java/animal/application/notification/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package animal.application.notification.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("animal.application.notification")
+public class PropertyConfig {
+
+}
