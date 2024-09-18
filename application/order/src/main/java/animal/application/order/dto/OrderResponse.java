c2ebb3c3310531508733297af619738d203590cf
barami62 <wodbs7758@naver.com>
2024-09-18 16:58:27 +0900

    feat(order): 허브 클라이언트 응답 DTO 추가

---
+++ application/order/src/main/java/animal/application/order/dto/OrderResponse.java
@@ -0,0 +1,25 @@
+package animal.application.order.dto;
+
+import animal.application.order.domain.delivery.Address;
+import animal.application.order.domain.delivery.Coordinate;
+import animal.application.order.domain.delivery.HubDeliveryManager;
+import java.util.List;
+import java.util.UUID;
+
+public class OrderResponse {
+
+    public record HubData(
+        List<HubInfo> data
+    ) {
+
+    }
+
+    public record HubInfo(
+        UUID id,
+        Address address,
+        Coordinate coordinate,
+        List<HubDeliveryManager> hubDeliveryManagerList
+    ) {
+
+    }
+}
