296f310ae352ea2e84dd1af356cdedcc42d751d0
barami62 <wodbs7758@naver.com>
2024-09-18 16:59:46 +0900

    feat(order): 허브 배송 담당자 지정 서비스 보강

--- application/order/src/main/java/animal/application/order/application/HubDeliveryService.java
+++ application/order/src/main/java/animal/application/order/application/HubDeliveryService.java
@@ -1,21 +1,37 @@
 package animal.application.order.application;
 
+import animal.application.order.domain.order.Order;
+import animal.application.order.dto.OrderResponse.HubInfo;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class HubDeliveryService {
+
+    private final HubReader hubReader;
+    private final OrderReader orderReader;
+    private final HubDeliveryAssigner hubDeliveryAssigner;
 
     /**
      * 매일 08:00에 허브별 배송 담당자 배정
      */
     @Scheduled(cron = "0 0 8 * * *") // 매일 08:00
+    @Transactional
     public void assignHubDeliveryToHubs() {
+        log.info("assignHubDeliveryToHubs");
         // 1. 배송 대기인 주문 조회
-        // 2. 배송 담당자 조회
-        // 3. 허브 조회
-        // 4. 허브별 배송 담당자 배정
+        List<Order> waitingOrderList = orderReader.getWaitingOrderList();
+
+        // 2. 배송 담당자 및 허브 조회
+        List<HubInfo> hubInfoList = hubReader.read();
+
+        // 3. 허브별 배송 담당자 배정
+        hubDeliveryAssigner.assign(waitingOrderList, hubInfoList);
     }
 }
