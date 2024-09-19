78d0fe6b853fdcf88f4654ae22abb8782512b691
SunMi <wywudi@naver.com>
2024-09-19 14:59:46 +0900

    fix : @Bean 삭제

--- application/gateway/src/main/java/animal/gateway/config/SecurityConfig.java
+++ application/gateway/src/main/java/animal/gateway/config/SecurityConfig.java
@@ -20,49 +20,48 @@ import security.JwtUtil;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  private static final Logger logger = Logger.getLogger(PreFilter.class.getName());
-  private final JwtUtil jwtUtil;
+    private static final Logger logger = Logger.getLogger(PreFilter.class.getName());
+    private final JwtUtil jwtUtil;
 
-  @Bean
-  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-    http
-        .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 비활성화
-        .addFilterAt(jwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC);
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+            .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 비활성화
+            .addFilterAt(jwtAuthenticationFilter(), SecurityWebFiltersOrder.HTTP_BASIC);
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public WebFilter jwtAuthenticationFilter() {
-    // TODO: 게이트웨이 jwt 인증 처리 필터
-    return (exchange, chain) -> {
+    public WebFilter jwtAuthenticationFilter() {
+        // TODO: 게이트웨이 jwt 인증 처리 필터
+        return (exchange, chain) -> {
 
-      // /auth/login 경로는 필터를 적용하지 않음
+            // /auth/login 경로는 필터를 적용하지 않음
 
-      String path = exchange.getRequest().getURI().getPath();
+            String path = exchange.getRequest().getURI().getPath();
 
-      logger.info("Pre filter :Request URI is" + path);
+            logger.info("Pre filter :Request URI is" + path);
 
-      //회원가입과 로그인 시 토큰 검증 하지 않음.
-      if (path.startsWith("/auth")) {
-        return chain.filter(exchange);
-      }
-      //토큰 추출
-      String token = jwtUtil.parseToken(exchange);
+            //회원가입과 로그인 시 토큰 검증 하지 않음.
+            if (path.startsWith("/auth")) {
+                return chain.filter(exchange);
+            }
+            //토큰 추출
+            String token = jwtUtil.parseToken(exchange);
 
-      // 토큰 유효성 검증과 만료기간 확인
-      Claims claims = jwtUtil.tokenParseClaims(token);
+            // 토큰 유효성 검증과 만료기간 확인
+            Claims claims = jwtUtil.tokenParseClaims(token);
 
-      //헤더에 값을 추가
-      ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
-          .header("X-User-Name", jwtUtil.getUsername(claims))
-          .header("X-User-Roles", jwtUtil.getRole(claims))
-          .build();
+            //헤더에 값을 추가
+            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate()
+                .header("X-User-Name", jwtUtil.getUsername(claims))
+                .header("X-User-Roles", jwtUtil.getRole(claims))
+                .build();
 
-      // 수정된 요청으로 필터 체인 계속 처리
-      ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
-      return chain.filter(modifiedExchange);
+            // 수정된 요청으로 필터 체인 계속 처리
+            ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+            return chain.filter(modifiedExchange);
 
-    };
-  }
+        };
+    }
 }
